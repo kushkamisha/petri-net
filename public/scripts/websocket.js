dfe36879c572f8c5d8dffd05a48f41179f30c752
Misha Kushka <kushka.misha@gmail.com>
2019-11-18 17:54:13 +0200

    Lab 8 completed
    Add task2 & task3 schemas

--- public/scripts/websocket.js
+++ public/scripts/websocket.js
@@ -2,7 +2,7 @@ const socket = new WebSocket('ws://127.0.0.1:16804')
 
 socket.onopen = () => {
     console.log('connected')
-    socket.send('net-data')
+    socket.send(JSON.stringify({ type: 'net-data' }))
 }
 
 socket.onclose = () => {
