1fd8f355bbb379a58b030f8b9c375dfab49a0616
Misha Kushka <kushka.misha@gmail.com>
2019-11-21 15:39:51 +0200

    Add save/load net functions

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,5 +1,23 @@
 'use strict'
 
 function saveNet() {
-    console.log(window.cy.json())
+    const net = window.cy.json().elements
+    const filename = 'PetriNet'
+    var blob = new Blob([JSON.stringify(net)], { type: 'text/json;charset=utf-8' })
+    saveAs(blob, filename + ".json")
+}
+
+function loadFromFile() {
+    const input = document.createElement('input')
+    input.type = 'file'
+
+    input.onchange = e => {
+        const file = e.target.files[0]
+        const reader = new FileReader()
+        reader.readAsText(file, "utf-8")
+
+        reader.onload = e => draw(JSON.parse(e.target.result))
+    }
+
+    input.click()
 }
