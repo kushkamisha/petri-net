e3d2eca80ae7716a2fc85ce96aa8224e7235bf0a
Misha Kushka <kushka.misha@gmail.com>
2019-11-22 10:28:54 +0200

    DON'T CHECKOUT TO THIS COMMIT
    Just some incompleted changes (unworking code)

--- public/js/functions.js
+++ public/js/functions.js
@@ -4,7 +4,7 @@ function saveNet() {
     const net = window.cy.json().elements
     const filename = 'PetriNet'
     var blob = new Blob([JSON.stringify(net)], { type: 'text/json;charset=utf-8' })
-    saveAs(blob, filename + ".json")
+    saveAs(blob, `${filename}.json`)
 }
 
 function loadFromFile() {
@@ -14,9 +14,15 @@ function loadFromFile() {
     input.onchange = e => {
         const file = e.target.files[0]
         const reader = new FileReader()
-        reader.readAsText(file, "utf-8")
+        reader.readAsText(file, 'utf-8')
 
-        reader.onload = e => draw(JSON.parse(e.target.result))
+        reader.onload = e => {
+            draw(JSON.parse(e.target.result))
+            socket.send(JSON.stringify({
+                type: 'recreate',
+                data: { filename: file.name, data: e.target.result }
+            }))
+        }
     }
 
     input.click()
