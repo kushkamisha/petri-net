4702c3feac689bfadad60bfd5a17461141f64123
Misha Kushka <kushka.misha@gmail.com>
2019-11-27 22:00:33 +0200

    When load a net - sets a cookie in user's browser
    Cookie points on the user's net on the server
    When the page is reloaded - all markers on the net go to the default position

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,5 +1,15 @@
 'use strict'
 
+function netExecute() {
+    const timestamp = getCookie('timestamp')
+    socket.send(JSON.stringify({ type: 'launch', ...timestamp && {timestamp} }))
+}
+
+function netNext() {
+    const timestamp = getCookie('timestamp')
+    socket.send(JSON.stringify({ type: 'next', ...timestamp && { timestamp } }))
+}
+
 function saveNet() {
     const net = window.cy.json().elements
     const filename = 'PetriNet'
@@ -17,13 +27,47 @@ function loadFromFile() {
         reader.readAsText(file, 'utf-8')
 
         reader.onload = e => {
+            // Draw a new net
             draw(JSON.parse(e.target.result))
+
+            // Save the file to the server & set a cookie
+            const timestamp = Date.now()
             socket.send(JSON.stringify({
                 type: 'recreate',
+                timestamp,
                 data: e.target.result
             }))
+            setCookie('timestamp', timestamp, 7)
         }
     }
 
     input.click()
+}
+
+/**
+ * Set a cookie in the user's browser
+ * @param {string} name - Name of the cookie
+ * @param {string} value - Value of the cookie
+ * @param {integer} days2live - Number of days before the cookie expires
+ */
+function setCookie(name, value, days2live) {
+    const d = new Date()
+    d.setTime(d.getTime() + days2live * 24 * 60 * 60 * 1000)
+    document.cookie = `${name}=${value}; expires=${d.toUTCString()}; path=/`
+}
+
+/**
+ * Get a cookie from the user's browser
+ * @param {string} name - Name of the cookie
+ */
+function getCookie(name) {
+    name = `${name}=`
+    const decodedCookie = decodeURIComponent(document.cookie)
+    const ca = decodedCookie.split(';')
+
+    for (let i = 0; i < ca.length; i++) {
+        const c = ca[i].trim()
+        if (c.indexOf(name) === 0) return c.substring(name.length)
+    }
+    return ''
 }
--- public/js/websocket.js
+++ public/js/websocket.js
@@ -2,7 +2,8 @@ const socket = new WebSocket('ws://127.0.0.1:16804')
 
 socket.onopen = () => {
     console.log('connected')
-    socket.send(JSON.stringify({ type: 'net-data' }))
+    const timestamp = getCookie('timestamp')
+    socket.send(JSON.stringify({ type: 'net-data', ...timestamp && {timestamp} }))
 }
 
 socket.onclose = () => {
