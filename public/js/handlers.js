b8d8b5f2094068005c8faf904b464429b2218a98
Misha Kushka <kushka.misha@gmail.com>
2019-12-05 22:35:55 +0200

    Draw arcs from GUI

--- public/js/handlers.js
+++ public/js/handlers.js
@@ -1,14 +1,32 @@
 'use strict'
 
+function isArc(trg) { return trg.isEdge() }
+
+function isPlace(trg) { return trg.data('type') === 'place' }
+
+function isTransition(trg) { return trg.data('type') === 'transition' }
+
+function isBackground(trg) { return trg === window.cy }
+
 function handleClicks() {
-    window.cy.on('tap', 'node', cyHandler)
-    window.cy.on('tap', 'edge', cyHandler)
-
     window.cy.on('tap', e => {
-        // Check is click was on the background
-        if (e.target !== window.cy) return
+        const trg = e.target
+        if (isBackground(trg)) return backgroundClick(e)
+        if (isTransition(trg)) return transitionClick(e)
+        if (isPlace(trg)) return placeClick(e)
+        if (isArc(trg)) return arcClick(e)
+    })
 
-        if (window.pressedKey === 'q')
+    window.addEventListener('keypress', keyHandler, false)
+    window.addEventListener('keyup', () => window.pressedKey = undefined)
+
+    arcDrawingHandler()
+}
+
+function backgroundClick(e) {
+    console.log('background click')
+    switch (window.pressedKey) {
+        case 'q':
             cy.add([{
                 group: 'nodes',
                 data: {
@@ -20,7 +38,8 @@ function handleClicks() {
                     y: e.renderedPosition.y,
                 },
             }])
-        else if (window.pressedKey === 'w')
+            break
+        case 'w':
             cy.add([{
                 group: 'nodes',
                 data: {
@@ -32,65 +51,15 @@ function handleClicks() {
                     y: e.renderedPosition.y,
                 },
             }])
-
-        console.log(cy.json().elements)
-    })
-
-    window.addEventListener('keypress', keyHandler, false)
-    window.addEventListener('keyup', () => window.pressedKey = undefined)
-}
-
-function cyHandler(event) {
-    // const type = event.target === window.cy ?
-    //     'background' :
-    //     event.target.data('type')
-    // console.log({ type })
-    const type = event.target.data('type')
-
-    switch (type) {
-        case 'place':
-            break
-        case 'transition':
-            const id = event.target.id()
-            transitionHandler(id)
-            break
-        default:
-            // arc
             break
     }
+
+    console.log(cy.json().elements)
 }
 
-function keyHandler(e) {
-    console.log(e.keyCode)
-    const keyCode = e.keyCode
-    const key = {
-        q: 113,
-        qUA: 1081,
-        qCAPS: 81,
-        qUACAPS: 1049,
-        w: 119,
-        wUA: 1094,
-        wCAPS: 87,
-        wUACAPS: 1062
-    }
-    
-    switch(keyCode) {
-        case key.q:
-        case key.qUA:
-        case key.qCAPS:
-        case key.qUACAPS:
-            window.pressedKey = 'q'
-            break
-        case key.w:
-        case key.wUA:
-        case key.wCAPS:
-        case key.wUACAPS:
-            window.pressedKey = 'w'
-            break
-    }
-}
-
-function transitionHandler(id) {
+function transitionClick(e) {
+    console.log('transition click')
+    const id = e.transition.id()
     let delay = prompt('Enter a time delay for the transition', 1)
     if (delay == null) return;
     while (isNaN(parseInt(delay)))
@@ -114,4 +83,142 @@ function transitionHandler(id) {
         timestamp,
         data
     }))
+}
+
+function placeClick(e) { console.log('place click') }
+
+function arcClick(e) { console.log('arc click') }
+
+function keyHandler(e) {
+    console.log(e.keyCode)
+    const keyCode = e.keyCode
+    const key = {
+        q: 113,
+        qUA: 1081,
+        qCAPS: 81,
+        qUACAPS: 1049,
+
+        w: 119,
+        wUA: 1094,
+        wCAPS: 87,
+        wUACAPS: 1062,
+
+        // e: 101,
+        // eUA: 1091,
+        // eCAPS: 69,
+        // eUACAPS: 1059
+    }
+    
+    switch(keyCode) {
+        case key.q:
+        case key.qUA:
+        case key.qCAPS:
+        case key.qUACAPS:
+            window.pressedKey = 'q'
+            break
+        case key.w:
+        case key.wUA:
+        case key.wCAPS:
+        case key.wUACAPS:
+            window.pressedKey = 'w'
+            break
+        // case key.e:
+        // case key.eUA:
+        // case key.eCAPS:
+        // case key.eUACAPS:
+        //     window.pressedKey = 'e'
+        //     break 
+    }
+}
+
+function arcDrawingHandler() {
+    const defaults = {
+        preview: true, // whether to show added edges preview before releasing selection
+        hoverDelay: 150, // time spent hovering over a target node before it is considered selected
+        handleNodes: 'node', // selector/filter function for whether edges can be made from a given node
+        snap: false, // when enabled, the edge can be drawn by just moving close to a target node
+        snapThreshold: 50, // the target node must be less than or equal to this many pixels away from the cursor/finger
+        snapFrequency: 15, // the number of times per second (Hz) that snap checks done (lower is less expensive)
+        noEdgeEventsInDraw: false, // set events:no to edges during draws, prevents mouseouts on compounds
+        disableBrowserGestures: true, // during an edge drawing gesture, disable browser gestures such as two-finger trackpad swipe and pinch-to-zoom
+        handlePosition: function (node) {
+            return 'middle top'; // sets the position of the handle in the format of "X-AXIS Y-AXIS" such as "left top", "middle top"
+        },
+        handleInDrawMode: false, // whether to show the handle in draw mode
+        edgeType: function (sourceNode, targetNode) {
+            // can return 'flat' for flat edges between nodes or 'node' for intermediate node between them
+            // returning null/undefined means an edge can't be added between the two nodes
+            return 'flat';
+        },
+        loopAllowed: function (node) {
+            // for the specified node, return whether edges from itself to itself are allowed
+            return false;
+        },
+        nodeLoopOffset: -50, // offset for edgeType: 'node' loops
+        nodeParams: function (sourceNode, targetNode) {
+            // for edges between the specified source and target
+            // return element object to be passed to cy.add() for intermediary node
+            return {};
+        },
+        edgeParams: function (sourceNode, targetNode, i) {
+            // for edges between the specified source and target
+            // return element object to be passed to cy.add() for edge
+            // NB: i indicates edge index in case of edgeType: 'node'
+            return {};
+        },
+        ghostEdgeParams: function () {
+            // return element object to be passed to cy.add() for the ghost edge
+            // (default classes are always added for you)
+            return {};
+        },
+        show: function (sourceNode) {
+            // fired when handle is shown
+        },
+        hide: function (sourceNode) {
+            // fired when the handle is hidden
+        },
+        start: function (sourceNode) {
+            // fired when edgehandles interaction starts (drag on handle)
+        },
+        complete: function (sourceNode, targetNode, addedEles) {
+            // fired when edgehandles is done and elements are added
+            console.log('new edge was added')
+        },
+        stop: function (sourceNode) {
+            // fired when edgehandles interaction is stopped (either complete with added edges or incomplete)
+        },
+        cancel: function (sourceNode, cancelledTargets) {
+            // fired when edgehandles are cancelled (incomplete gesture)
+        },
+        hoverover: function (sourceNode, targetNode) {
+            // fired when a target is hovered
+            checkEdgeTarget(sourceNode, targetNode)
+        },
+        hoverout: function (sourceNode, targetNode) {
+            // fired when a target isn't hovered anymore
+        },
+        previewon: function (sourceNode, targetNode, previewEles) {
+            // fired when preview is shown
+        },
+        previewoff: function (sourceNode, targetNode, previewEles) {
+            // fired when preview is hidden
+        },
+        drawon: function () {
+            // fired when draw mode enabled
+        },
+        drawoff: function () {
+            // fired when draw mode disabled
+        }
+    }
+    window.eh = window.cy.edgehandles(defaults)
+    window.eh.enableDrawMode()
+}
+
+function checkEdgeTarget(src, trg) {
+    // Check is arc is valid
+    // console.log(trg)
+    if (isTransition(src) && isPlace(trg) || isPlace(src) && isTransition(trg))
+        console.log('the arc is valid')
+    else
+        console.log('the arc is invalid')
 }
